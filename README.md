b747ccd4bbe3e3193f000de6d6e09dc707ef7c86
LucaArgentieri <lucaargentieri-98@hotmail.it>
2025-01-31 22:49:35 +0100

    create new features + handle internal logic

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Video Player - Custom Component
-#### Custom component version of Vue Video Player, the scope of this project is to explore Web components and make a "frameworkless" element.
+#### Custom component version of Vue Video Player, the scope of this project is to explore Web components and make an agnostic/"frameworkless" element.
 
 #### High prio:
-- [ ] Handle player logic (take it from Vue Video Player)
-- [ ] Find a way to control functions like play and mute from external (maybe data attributes?)
-- [ ] Find a way to overwrite/customize css from external
+- [x] Handle player logic (take it from Vue Video Player)
+- [x] Find a way to control functions like play and mute from external (maybe data attributes?)
+- [ ] Find a way to overwrite/customize css from external keeping logic internal
 
 #### Low prio:
 - [ ] Documentation
--- js/index.js
+++ js/index.js
@@ -2,6 +2,15 @@ class VideoPlayer extends HTMLElement {
     constructor() {
         super();
         this.video = null;
+        this.playButton = null;
+        this.muteButton = null;
+        this.fullscreenButton = null;
+        this.pipButton = null;
+        this.progressBar = null;
+        this.durationElement = null;
+        this.currentTimeElement = null;
+        this.duration = 0;
+        this.progressValue = 0;
         this.muted = this.hasAttribute('muted') || false;
         this.autoplay = this.hasAttribute('autoplay') || false;
         this.controls = this.hasAttribute('controls') || false;
@@ -11,29 +20,11 @@ class VideoPlayer extends HTMLElement {
         this.poster = this.getAttribute('poster') || '';
         this.preload = this.getAttribute('preload') || 'auto';
         this.playsinline = this.hasAttribute('playsinline');
-        this.sources = this.getAttribute('sources') ? JSON.parse(this.getAttribute('sources')) : [];
 
+        //Shadow DOM
         const shadow = this.attachShadow({ mode: 'open' });
-
-        this.init(shadow)
-        this.events()
-        this.setVideoSlotSources()
-
-    }
-    init(shadow) {
         shadow.innerHTML = `
-          <style>
-            :host {
-
-            }
-            video {
-                width: 100%;
-                height: auto;
-                aspect-ratio: 16 / 9;
-            }
-          </style>
-            <div class="player-video__wrapper">
-                <video 
+                <video data-video-player="video"
                     ${this.muted && 'muted'}
                     ${this.autoplay && 'autoplay'}
                     ${this.controls && 'controls'}
@@ -47,19 +38,20 @@ class VideoPlayer extends HTMLElement {
                 </video>
                 <slot name="video" style="display: none;"></slot> 
                 <slot name="controls"></slot>
-            </div>
         `;
     }
-    events() {
-        this.shadowRoot.addEventListener('click', (event) => {
-            if (event.target.tagName === 'VIDEO') {
-                this.clickOnVideo();
-            }
-        });
+    connectedCallback() {
+        this.video = this.shadowRoot.querySelector('[data-video-player="video"]');
+        this.playButton = this.querySelector('[data-video-player="play-button"]');
+        this.muteButton = this.querySelector('[data-video-player="mute-button"]');
+        this.fullscreenButton = this.querySelector('[data-video-player="fullscreen-button"]');
+        this.pipButton = this.querySelector('[data-video-player="pip-button"]');
+        this.progressBar = this.querySelector('[data-video-player="progress-bar"]');
+        this.durationElement = this.querySelector('[data-video-player="duration"]');
+        this.currentTimeElement = this.querySelector('[data-video-player="current-time"]');
 
-    }
-    clickOnVideo() {
-        console.log('clickOnVideo');
+        this.setVideoSlotSources();
+        this.events();
     }
     setVideoSlotSources() {
         const slot = this.shadowRoot.querySelector('slot[name="video"]');
@@ -75,8 +67,63 @@ class VideoPlayer extends HTMLElement {
             });
         });
     }
-
+    events() {
+        this.video.addEventListener('loadedmetadata', () => {
+            this.duration = this.video.duration;
+            if (this.currentTimeElement) this.currentTimeElement.textContent = `00:00`
+            if (this.durationElement) this.durationElement.textContent = this.convertTimeToDuration(this.duration);
+            if (this.progressBar) this.progressValue;
+        });
+        this.video.addEventListener('timeupdate', () => {
+            this.progressValue = (this.video.currentTime / this.video.duration) * 100
+            if (this.currentTimeElement) this.currentTimeElement.textContent = this.convertTimeToDuration(this.video.currentTime);
+            if (this.progressBar) this.progressBar.value = this.progressValue;
+        });
+        this.video.addEventListener('click', () => {
+            this.toggleVideoPlay();
+        })
+        this.playButton.addEventListener('click', () => {
+            this.toggleVideoPlay();
+        })
+        this.muteButton.addEventListener('click', () => {
+            this.toggleVideoMute();
+        })
+        this.fullscreenButton.addEventListener('click', () => {
+            this.toggleFullscreen();
+        })
+        this.pipButton.addEventListener('click', () => {
+            this.togglePip();
+        })
+        this.progressBar.addEventListener('input', (event) => {
+            this.video.currentTime = (event.target.value / 100) * this.video.duration;
+        })
+    }
+    toggleVideoPlay() {
+        this.video.paused ? this.video.play() : this.video.pause();
+        this.playButton.classList.toggle('paused');
+    }
+    toggleVideoMute() {
+        this.video.muted = !this.video.muted;
+        this.muteButton.classList.toggle('muted');
+    }
+    toggleFullscreen() {
+        if (this.video.requestFullscreen) {
+            this.video.requestFullscreen();
+        } else if (this.video.webkitRequestFullscreen) {
+            this.video.webkitRequestFullscreen();
+        } else if (this.video.msRequestFullscreen) {
+            this.video.msRequestFullscreen();
+        }
+    }
+    togglePip() {
+        if (this.video.requestPictureInPicture) {
+            this.video.requestPictureInPicture();
+        }
+    }
+    convertTimeToDuration(seconds) {
+        return [Math.floor((seconds / 60) % 60), Math.floor(seconds % 60)]
+            .map(num => num.toString().padStart(2, "0"))
+            .join(":");
+    }
 }
-
-
-customElements.define('player-video', VideoPlayer);
+customElements.define('video-player', VideoPlayer);
